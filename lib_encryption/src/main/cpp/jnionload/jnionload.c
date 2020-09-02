e1d21c206333eb3f0a1e9f05d460433871af578f
Administrator <153437803@qq.com>
2020-09-02 12:49:35 +0800

    整理文件结构

--- lib_encryption/src/main/cpp/jnionload/jnionload.c
+++ lib_encryption/src/main/cpp/jnionload/jnionload.c
@@ -2,7 +2,6 @@
 #include <string.h>
 #include "jnionload.h"
 #include "../cipher/ciphertool.h"
-#include "../multidex/multidex.h"
 #include "../tool/applicationtool.h"
 
 JNIEXPORT JNICALL jint JNI_OnLoad(JavaVM *vm, void *reserved) {
@@ -10,20 +9,6 @@ JNIEXPORT JNICALL jint JNI_OnLoad(JavaVM *vm, void *reserved) {
     JNIEnv *env = NULL;
 
     if ((*vm)->GetEnv(vm, (void **) &env, JNI_VERSION_1_6) != JNI_OK) {
-        return JNI_ERR;
-    }
-
-    // multidex
-    jclass multidex_clazz = (*env)->FindClass(env, "lib/kalu/encryption/multidex/BoostNative");
-    static JNINativeMethod multidex_methods[] = {
-            {"obtainCheckSum", "(Ljava/lang/String;)J",                    (void *) JNI_OnLoad_ObtainCheckSum},
-            {"makeOptDexFile", "(Ljava/lang/String;Ljava/lang/String;)Z",  (void *) JNI_OnLoad_MakeOptDexFile},
-            {"initialize",     "(ILjava/lang/Class;)Z",                    (void *) JNI_OnLoad_Initialize},
-            {"recoverAction",  "()V",                                      (void *) JNI_OnLoad_RecoverAction},
-            {"loadDirectDex", "(Ljava/lang/String;[B)Ljava/lang/Object;", (void *) JNI_OnLoad_LoadDirectDex},
-    };
-    jint multidex_size = sizeof(multidex_methods) / sizeof(multidex_methods[0]);
-    if ((*env)->RegisterNatives(env, multidex_clazz, multidex_methods, multidex_size) != JNI_OK) {
         return JNI_ERR;
     }
 
@@ -51,34 +36,4 @@ jstring
 JNI_OnLoad_AesEncode(JNIEnv *env, jobject instance, jstring jstr) {
 
     return aesEncode(env, instance, jstr);
-}
-
-jlong
-JNI_OnLoad_ObtainCheckSum(JNIEnv *env, jobject instance, jstring jstr) {
-
-    return obtainCheckSum(env, instance, jstr);
-}
-
-jobject
-JNI_OnLoad_LoadDirectDex(JNIEnv *env, jobject instance, jstring jstr, jbyte jbyte) {
-
-    return loadDirectDex(env, instance, jstr, jbyte);
-}
-
-void
-JNI_OnLoad_RecoverAction(JNIEnv *env, jobject instance) {
-
-    recoverAction(env, instance);
-}
-
-jboolean
-JNI_OnLoad_MakeOptDexFile(JNIEnv *env, jobject instance, jstring filePath, jstring optFilePath) {
-
-    return makeOptDexFile(env, instance, filePath, optFilePath);
-}
-
-jboolean
-JNI_OnLoad_Initialize(JNIEnv *env, jobject instance, jint sdkVersion, jclass jcls) {
-
-    return initialize(env, instance, sdkVersion, jcls);
 }
--- lib_encryption/src/main/cpp/jnionload/jnionload.h
+++ lib_encryption/src/main/cpp/jnionload/jnionload.h
@@ -4,24 +4,8 @@
 #ifndef JNI_ONLOAD_H
 #define JNI_ONLOAD_H
 
-////////////////////////////////////////////////////////////////////////////////
-
 jstring JNI_OnLoad_AesDecode(JNIEnv *env, jobject instance, jstring jstr);
 
 jstring JNI_OnLoad_AesEncode(JNIEnv *env, jobject instance, jstring jstr);
-
-////////////////////////////////////////////////////////////////////////////////
-
-jobject JNI_OnLoad_LoadDirectDex(JNIEnv *env, jobject instance, jstring jstr, jbyte jbyte);
-
-jlong JNI_OnLoad_ObtainCheckSum(JNIEnv *env, jobject instance, jstring jstr);
-
-void JNI_OnLoad_RecoverAction(JNIEnv *env, jobject instance);
-
-jboolean JNI_OnLoad_MakeOptDexFile(JNIEnv *env, jobject instance, jstring filePath, jstring optFilePath);
-
-jboolean JNI_OnLoad_Initialize(JNIEnv *env, jobject instance, jint sdkVersion, jclass runtimeExceptionClass);
-
-////////////////////////////////////////////////////////////////////////////////
 
 #endif
